091b30c20ef76c3b56516e80d6e0c497224e7cde
fran-exfer <fran.exfer@gmail.com>
2021-04-05 04:07:14 +0200

    Fix note card link not filling all the vertical space.

--- src/components/Note.js
+++ src/components/Note.js
@@ -3,14 +3,15 @@ import { Link } from 'react-router-dom';
 
 export default function Note({ note }) {
   return (
-    <article className="bg-white hover:bg-blue-50 cursor-pointer transition-all rounded-lg p-4 shadow-md border border-gray-200 | w-full md:max-w-xs max-h-60 | md:mr-8 mb-8">
-      <Link to={`/note/${note.id}`}>
-        <h1 className="text-xl mb-3">{note.title}</h1>
-        <p>
-          {note.content.substring(0, 200).trim()}
-          {note.content.length > 200 ? '...' : ''}
-        </p>
-      </Link>
-    </article>
+    <Link
+      to={`/note/${note.id}`}
+      className="bg-white hover:bg-blue-50 cursor-pointer transition-all rounded-lg p-4 shadow-md border border-gray-200 | w-full md:max-w-xs max-h-60 | md:mr-8 mb-8"
+    >
+      <h1 className="text-xl mb-3">{note.title}</h1>
+      <p>
+        {note.content.substring(0, 200).trim()}
+        {note.content.length > 200 ? '...' : ''}
+      </p>
+    </Link>
   );
 }
