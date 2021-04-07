5d1075b7c5ee4ef6f99779440ef81cd4b7ce8c14
fran-exfer <fran.exfer@gmail.com>
2021-04-07 23:30:38 +0200

    Reimplement localStorage. Refactor complete.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useReducer } from 'react';
+import { useReducer, useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import notoReducer from './utils/notoReducer';
@@ -9,19 +9,34 @@ import NoteList from './components/NoteList';
 import NoteModal from './components/NoteModal';
 
 export default function App() {
-  // We'll use a reducer to centralize and abstract state-modifying actions.
+  // Get initial state from localStorage or initialize it
+  const loadInitialState = () => {
+    const storageNotes = window.localStorage.getItem('NotoNoto_notes');
+    const storageTheme = window.localStorage.getItem('NotoNoto_theme');
+
+    return {
+      notes: storageNotes === null ? [] : JSON.parse(storageNotes),
+      theme: storageTheme === null ? 'light' : storageTheme,
+    };
+  };
 
   /*
+   * We'll use a reducer to centralize and abstract state-modifying actions.
    * We'll use React Context API to provide this reducer logic to all components
    * that need it.
    */
-  const centralState = useReducer(notoReducer, {
-    notes: [
-      { id: 2342424, title: 'Test note', content: 'Test content blabla' },
-    ],
-    theme: 'light',
-  });
+  const centralState = useReducer(notoReducer, loadInitialState());
+
   const [state] = centralState;
+  console.log(state);
+
+  /*
+   * Save to localStorage whenever the state changes
+   */
+  useEffect(() => {
+    window.localStorage.setItem('NotoNoto_notes', JSON.stringify(state.notes));
+    window.localStorage.setItem('NotoNoto_theme', state.theme);
+  }, [state]);
 
   /*
    * Theming is done by appending the theme value from the state as a CSS
