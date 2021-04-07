b222004b3188c59701db0a04b887007b22483703
fran-exfer <fran.exfer@gmail.com>
2021-04-07 20:19:53 +0200

    Reimplement NoteModal's fill up form when specifying a valid note id in the browser route.

--- src/components/NoteModal.js
+++ src/components/NoteModal.js
@@ -1,10 +1,31 @@
-import { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { useContext, useEffect, useRef } from 'react';
+import { Link, useHistory, useParams } from 'react-router-dom';
 
 import NotoContext from '../utils/NotoContext';
 
 export default function NoteModal() {
   const [state] = useContext(NotoContext);
+  const history = useHistory();
+  const { id } = useParams();
+
+  const titleInput = useRef('');
+  const contentTextarea = useRef('');
+
+  /*
+   * If there's a note id in the url, we fill up the form.
+   */
+  useEffect(() => {
+    if (!id) return;
+    const selected = state.notes.find((note) => note.id === Number(id));
+
+    if (!selected) {
+      history.push('/');
+      return;
+    }
+
+    titleInput.current.value = selected.title;
+    contentTextarea.current.value = selected.content;
+  }, [id, history, state.notes]);
 
   return (
     <div className="note-modal">
@@ -18,12 +39,14 @@ export default function NoteModal() {
             type="text"
             placeholder="Note title..."
             className={`note-modal__note-title ${state.theme}`}
+            ref={titleInput}
           />
           <textarea
             required
             rows="10"
             placeholder="Note content..."
             className="note-modal__note-content"
+            ref={contentTextarea}
           ></textarea>
           <div className="note-modal__controls">
             <input
@@ -31,16 +54,14 @@ export default function NoteModal() {
               value="Save note"
               className="btn btn--primary"
             />
-            {/* TODO LATER
             {id && (
               <button
-                className="btn btn-delete"
-                onClick={(e) => handleDelete(e, id, history)}
+                className="btn btn--danger"
+                onClick={(e) => e.preventDefault}
               >
                 Delete
               </button>
-            )
-            */}
+            )}
           </div>
         </form>
       </article>
